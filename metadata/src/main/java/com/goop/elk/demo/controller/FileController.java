759207a34b1d20fb996079a405ff2edb3af3fac6
Andrei Perevozchikov <goopnigoop@gmail.com>
2020-03-23 21:35:40 +0300

    + sleuth

--- metadata/src/main/java/com/goop/elk/demo/controller/FileController.java
+++ metadata/src/main/java/com/goop/elk/demo/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.goop.elk.demo.controller;
 
+import com.google.gson.Gson;
 import com.goop.elk.demo.converter.FileEntityMapper;
 import com.goop.elk.demo.dto.FileEntityDto;
 import com.goop.elk.demo.dto.ResponseWrapper;
@@ -16,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -41,10 +41,12 @@ public class FileController {
 
     @GetMapping("/{filename}")
     public ResponseWrapper<List<FileEntityDto>> getFileByName(@PathVariable("filename") String filename) {
+        logger.info("Loading metadata for file: {}", filename);
         final List<FileEntityDto> listOfEntries = fileEntityService.findByFileName(filename)
                 .stream()
                 .map(fileEntityMapper::modelToDto)
                 .collect(Collectors.toList());
+        logger.info("metadata is found: {}", new Gson().toJson(listOfEntries));
         return new ResponseWrapper<>(environment,listOfEntries);
     }
 
