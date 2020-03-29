c99e0a70a58f8cb44d4390228284265c3dee30fe
Andrei Perevozchikov <drook@yandex.ru>
2020-03-29 20:24:18 +0300

    + zipkin info (#15)

--- file/src/main/java/com/goop/file/controller/FileController.java
+++ file/src/main/java/com/goop/file/controller/FileController.java
@@ -8,8 +8,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +20,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/file")
+@Slf4j
 public class FileController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(FileController.class);
-
     public FileController(FileMetadataService fileMetadataService) {
         this.fileMetadataService = fileMetadataService;
     }
@@ -31,17 +29,17 @@ public class FileController {
     private FileMetadataService fileMetadataService;
 
     @GetMapping("/{fileName}")
-    @HystrixCommand(fallbackMethod = "fallback")
+    @HystrixCommand(commandKey = "getFiles",fallbackMethod = "fallback")
     ResponseEntity<?> getFileInformation(@PathVariable String fileName) {
-        LOGGER.info("getting information about file: {}", fileName);
+        log.info("getting information about file: {}", fileName);
         final ResponseWrapper<List<FileEntityDto>> fileMetadataByFilename = fileMetadataService.getFileMetadataByFilename(fileName);
         Gson gson = new Gson();
-        LOGGER.info("Metadata is received: {}", gson.toJson(fileMetadataByFilename));
+        log.info("Metadata is received: {}", gson.toJson(fileMetadataByFilename));
         return ResponseEntity.ok(fileMetadataByFilename);
     }
 
     public ResponseEntity<?> fallback(String fileName, Throwable hystrixCommand) {
-        LOGGER.warn("Something happens with service: {}", hystrixCommand.getMessage());
+        log.warn("Something happens with service: {}", hystrixCommand.getMessage());
         return new ResponseEntity<>(new ThrowableResponseWrapper("Service is unreachable",hystrixCommand),HttpStatus.SERVICE_UNAVAILABLE);
     }
 
